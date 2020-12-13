5fef4d4e2076cb8e1dbd6c7e6c12bf17f134ae49
Hung <mqhung93@gmail.com>
2020-12-13 10:11:13 +0700

    tong duong cheo trong mang

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int [][]arr = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println("Sum Diagonal Line is: " + sumDiagonal(arr));
+
+    }
+    public static int sumDiagonal(int [][]arr){
+        int sum =0;
+        for (int i=0; i<arr.length;i++){
+            sum += arr[i][i];
+        }
+        return sum;
+    }
+}
